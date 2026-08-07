2e47312267cfa67e7e0a590b8ca25844cfed3f74
kyleghughes <kyle.hughes123@hotmail.co.uk>
2026-08-07 22:08:43 +0100

    Added Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
+      <Analytics />
       <App />
     </StrictMode>
   </QueryClientProvider>,
